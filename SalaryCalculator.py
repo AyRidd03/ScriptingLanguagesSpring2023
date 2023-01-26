ef8ca83100e13fda63b2492b8f2ceb06dc7bfca7
AyRidd03 <ayridd03@gmail.com>
2023-01-25 18:32:27 -0600

    Created the Variables DASH_LENGTH, COLUMN_LENGTH, and input salary_per_hour. KNOWN WARNINGS: There is an extra line at the end of the code I will be using to start creating the scripts for the output.

--- SalaryCalculator.py
+++ SalaryCalculator.py
@@ -1,1 +1,10 @@
 # Salary Calculator by Ayden Riddle
+
+# Variables
+
+DASH_LENGTH = 40  # To be used for repeating Dashes in code
+COLUMN_LENGTH = 25  # To be used when setting a standard column length
+salary_per_hour = float(input(f"{'Salary per hour':.<{COLUMN_LENGTH}}"))
+
+# Main Void Actions? (Still learning the terminology, just console commands that perform an output)
+
