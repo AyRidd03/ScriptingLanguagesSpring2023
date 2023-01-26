f7b2cf297b3a68332682c7186fbc8b605a8621d3
AyRidd03 <ayridd03@gmail.com>
2023-01-25 18:58:57 -0600

    Readjusted the labeling to more appropriately fit the file. Finished the inputs with hours_per_week, days_per_week, holidays_per_year,  and vacations_per_year. Created the Intro Display
    What to Finish: Calculations and the final pieces of the end display

--- SalaryCalculator.py
+++ SalaryCalculator.py
@@ -4,7 +4,19 @@
 
 DASH_LENGTH = 40  # To be used for repeating Dashes in code
 COLUMN_LENGTH = 25  # To be used when setting a standard column length
-salary_per_hour = float(input(f"{'Salary per hour':.<{COLUMN_LENGTH}}"))
 
-# Main Void Actions? (Still learning the terminology, just console commands that perform an output)
+# Intro Display
 
+print('=' * DASH_LENGTH)
+print("The Annual Salary Calculator")
+print('=' * DASH_LENGTH)
+
+# Inputs
+
+salary_per_hour = float(input(f"{'Salary per Hour':.<{COLUMN_LENGTH}}"))  # Takes input from user of Salary per Hour
+hours_per_week = float(input(f"{'Hours per Week':.<{COLUMN_LENGTH}}"))  # Takes input for Hours per Week
+days_per_week = float(input(f"{'Days per Week':.<{COLUMN_LENGTH}}"))  # Takes input Days per Week
+holidays_per_year = float(input(f"{'Holidays per Year':.<{COLUMN_LENGTH}}"))  # Takes Holidays per Year
+vacations_per_year = float(input(f"{'Vacation Days per Year':.<{COLUMN_LENGTH}}"))  # Takes Vacation Days Per Year
+
+# Calculations
