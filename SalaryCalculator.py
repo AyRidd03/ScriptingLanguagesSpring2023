6f35f7a01a715fa6c837672a774aa1e528a7caa7
AyRidd03 <ayridd03@gmail.com>
2023-01-25 19:21:57 -0600

    Almost Finished, but... I believe that there is a calculation error I will have to attend to. Specific Error pending, needs debugging.

--- SalaryCalculator.py
+++ SalaryCalculator.py
@@ -4,6 +4,7 @@
 
 DASH_LENGTH = 40  # To be used for repeating Dashes in code
 COLUMN_LENGTH = 25  # To be used when setting a standard column length
+WEEKS_PER_YEAR = 52
 
 # Intro Display
 
@@ -20,3 +21,17 @@ holidays_per_year = float(input(f"{'Holidays per Year':.<{COLUMN_LENGTH}}"))  # 
 vacations_per_year = float(input(f"{'Vacation Days per Year':.<{COLUMN_LENGTH}}"))  # Takes Vacation Days Per Year
 
 # Calculations
+
+unadjusted_salary = salary_per_hour * hours_per_week * days_per_week * WEEKS_PER_YEAR  # Calculates unadjusted Salary
+
+adjusted_days_per_year = days_per_week * WEEKS_PER_YEAR - holidays_per_year - vacations_per_year  # Calculates Days
+adjusted_salary = salary_per_hour * hours_per_week * adjusted_days_per_year  # Calculates adjusted Salary
+
+# End Display
+
+print('=' * DASH_LENGTH)
+print(f"{'Unadjusted Salary':.<{COLUMN_LENGTH}}: ${unadjusted_salary:6,.2f}")
+print(f"{'Adjusted Salary':.<{COLUMN_LENGTH}}: ${adjusted_salary:6,.2f}")
+print('=' * DASH_LENGTH)
+print("Thank You For Using Annual Salary Calculations,")
+print("Have a Nice Day!")
