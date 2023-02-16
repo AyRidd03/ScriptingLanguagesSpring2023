189e75e64b253e5af805fe140c49abc201419049
AyRidd03 <ayridd03@gmail.com>
2023-02-16 10:08:57 -0600

    Started Method add_student Known Issues: > add_student doesn't add a student yet > Any other input seems to crash the code except 0 and 2 > The warnings that stuff isn't being used

---
+++ Student Record System/student_maintenance.py
@@ -0,0 +1,5 @@
+
+def add_student(studentlist, newstudentid):
+    studentfname = input("Please insert the Student's First Name: ")  # To store first name into the student id later
+    studentlname = input("Please insert the Student's Last Name: ")  # To store last name into the student id later
+    print(f"'Student', {studentfname}, {studentlname}, 'has been added'")
