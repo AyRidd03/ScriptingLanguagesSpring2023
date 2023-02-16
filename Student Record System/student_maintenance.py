495b039a98097d6a5979fd867a24d4e8595a27e2
AyRidd03 <ayridd03@gmail.com>
2023-02-16 10:54:50 -0600

    Finished add_student started list_students: > added flavor functions to prevent crashes. > Fixed credentials, since I'm not editing the preset code Known Issues: > list_students crashes program due to failure to unpack > id in list_students appears to have shadows built-in

--- Student Record System/student_maintenance.py
+++ Student Record System/student_maintenance.py
@@ -1,5 +1,40 @@
+#!/usr/bin/env python3
 
-def add_student(studentlist, newstudentid):
-    studentfname = input("Please insert the Student's First Name: ")  # To store first name into the student id later
-    studentlname = input("Please insert the Student's Last Name: ")  # To store last name into the student id later
-    print(f"'Student', {studentfname}, {studentlname}, 'has been added'")
+# the following are module level dunders (metadata) for the authorship information
+__author__ = 'Ayden Riddle'
+__version__ = '1.0'
+__date__ = '2023.02.16'
+__status__ = 'Development'
+
+LINE = "=" * 20  # For Neatness :)
+
+
+def add_student(student_list, new_student_id):
+    print(LINE)
+    student_f_name = input("Please insert the Student's First Name: ")  # To store first name into the student id later
+    student_l_name = input("Please insert the Student's Last Name: ")  # To store last name into the student id later
+    student_list.update({new_student_id: (student_f_name, student_l_name)})
+    print(f"Student {student_f_name}, {student_l_name} has been added.")  # To Let the User Know
+    print(LINE)
+
+
+def list_students(student_list):
+    print(LINE)
+    print("Here's Your List of Students: ")
+    for id, student in student_list:  # Unpacks the Dictionary
+        for f_name, l_name in student.values():  # Unpacks the Tuple in the Dictionary
+            print(f"{id}: {f_name},{l_name}")
+
+
+def delete_student(student_list):
+    print(LINE)
+    student_delete_id = input("Please insert the Student ID you wish to Delete: ")  # To let the user pick the ID
+    user_confirmation = input("Are you Sure this is the Student you wish to delete? Y/N: ")  # To Doublecheck
+    print("This Student has been deleted.")  # To Let the User Know
+    print(LINE)
+
+
+def update_student(student_list):
+    student_edit_id = input("Please insert the Student ID you wish to Edit: ")  # To let the user pick the ID
+    print("This Student has been updated")  # To Let the User Know
+    print(LINE)
