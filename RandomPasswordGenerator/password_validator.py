ba0ebce866afe3cccc23f076ace7a920b118123a
AyRidd03 <ayridd03@gmail.com>
2023-04-13 09:51:16 -0500

    Finished Password Validator but not generator:         -Made the other Minimum requirement testers         -Cleaned up the presentation Things Needed Done:     -Set up the password generator     -(optional) Make an array of passwords     -(optional) Clean up code to write outside of method

--- RandomPasswordGenerator/password_validator.py
+++ RandomPasswordGenerator/password_validator.py
@@ -1,7 +1,11 @@
 class PasswordValidator:
+    LINE = "=" * 30
     __MIN_NUMBERS = 2
     __MIN_CAP_LETTERS = 2
     __MIN_LOW_LETTERS = 2
+    __MIN_SPEC_LETTERS = 2
+    __ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    __NUMBERS = "0123456789"
 
     def __init__(self, password=None, debug=False):
         self.password = password
@@ -9,21 +13,85 @@ class PasswordValidator:
 
     def is_valid(self, password):
         self.password = password
+        print(PasswordValidator.LINE)
         if self.password is None:
             print("Password has no Value")
+            print(PasswordValidator.LINE)
+            return
         else:
             print(f"Here is your password: {self.password}")
+        print(PasswordValidator.LINE)
+
         if self.is_caps_valid():
             print("Caps Valid")
         else:
             print("Not Caps Valid")
+        print(PasswordValidator.LINE)
+
+        if self.is_low_valid():
+            print("Low Valid")
+        else:
+            print("Not Low Valid")
+        print(PasswordValidator.LINE)
+
+        if self.is_num_valid():
+            print("Num Valid")
+        else:
+            print("Not Num Valid")
+        print(PasswordValidator.LINE)
+
+        if self.is_spec_valid():
+            print("Spec Valid")
+        else:
+            print("Not Spec Valid")
+        print(PasswordValidator.LINE)
 
     def is_caps_valid(self):
         count = 0
         for i in self.password:
             if i.isupper():
                 count += 1
+        print(f"Your password has {count} CAPITAL LETTER(S)")
+        print(f"You need {PasswordValidator.__MIN_CAP_LETTERS} CAPITAL LETTER(S)")
         if count >= PasswordValidator.__MIN_CAP_LETTERS:
+
+            return True
+        else:
+            return False
+
+    def is_low_valid(self):
+        count = 0
+        for i in self.password:
+            if i.islower():
+                count += 1
+        print(f"Your password has {count} lowercase letter(s)")
+        print(f"You need {PasswordValidator.__MIN_LOW_LETTERS} lowercase letter(s)")
+        if count >= PasswordValidator.__MIN_LOW_LETTERS:
+
+            return True
+        else:
+            return False
+
+    def is_num_valid(self):
+        count = 0
+        for i in self.password:
+            if i in PasswordValidator.__NUMBERS:
+                count += 1
+        print(f"Your password has {count} Number(s)")
+        print(f"You need {PasswordValidator.__MIN_NUMBERS} Number(s)")
+        if count >= PasswordValidator.__MIN_NUMBERS:
+            return True
+        else:
+            return False
+
+    def is_spec_valid(self):
+        count = 0
+        for i in self.password:
+            if i not in PasswordValidator.__ALPHABET and i not in PasswordValidator.__NUMBERS:
+                count += 1
+        print(f"Your password has {count} Special Character(s)")
+        print(f"You need {PasswordValidator.__MIN_SPEC_LETTERS} Special Character(s)")
+        if count >= PasswordValidator.__MIN_SPEC_LETTERS:
             return True
         else:
             return False
